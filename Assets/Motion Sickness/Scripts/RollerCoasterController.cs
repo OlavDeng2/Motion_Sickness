8c2e6f2b0799231b658578971d3443b9e6893a36
OlavDeng2 <olavkruithof@gmail.com>
2019-10-12 14:41:45 +0200

    The rollercoaster now moves

--- Assets/Motion Sickness/Scripts/RollerCoasterController.cs
+++ Assets/Motion Sickness/Scripts/RollerCoasterController.cs
@@ -7,17 +7,20 @@ public class RollerCoasterController : MonoBehaviour
     //Path to follow
     public PathCreation.PathCreator path;
     private bool hasStarted = false;
-    private float currentSpeed = 0;
+    private float currentSpeed = 1;
+    private float distanceTraveled = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distanceTraveled += currentSpeed * Time.deltaTime;
+        transform.position = path.path.GetPointAtDistance(distanceTraveled);
+        transform.rotation = path.path.GetRotationAtDistance(distanceTraveled);
+
     }
 }
