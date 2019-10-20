c283f6de9c2847e29fc9b9f2c05781884754d604
OlavDeng2 <olavkruithof@gmail.com>
2019-10-20 13:38:38 +0200

    boop 2

--- Assets/Motion Sickness/Scripts/SpeedVsDistance.cs
+++ Assets/Motion Sickness/Scripts/SpeedVsDistance.cs
@@ -2,12 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpeedPoints
-{
-    public float distance;
-    public float speed;
-}
-
 public class SpeedVsDistance : MonoBehaviour
 {
     public int[] distance;
