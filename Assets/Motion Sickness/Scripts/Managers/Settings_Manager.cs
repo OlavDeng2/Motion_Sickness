c18900b499ea86eed9e2cb2a261be46024d35668
OlavDeng2 <olavkruithof@gmail.com>
2019-11-21 12:27:09 +0100

    set the base of loading the VR settings

--- Assets/Motion Sickness/Scripts/Managers/Settings_Manager.cs
+++ Assets/Motion Sickness/Scripts/Managers/Settings_Manager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Settings_Manager : MonoBehaviour
 {
+    //1 = true, 0 = false
+    public int VRNose = 0;
+    public int lowFoV = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,11 @@ public class Settings_Manager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void SaveSettings()
+    {
+        PlayerPrefs.SetInt("VRNose", VRNose);
+        PlayerPrefs.SetInt("lowFoV", lowFoV);
     }
 }
---
+++ Assets/Motion Sickness/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Motion Sickness Settings")]
+    bool VRNose = false;
+    bool lowFoV = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        LoadSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //get and set the settings for motion sickness
+    private void LoadSettings()
+    {
+        VRNose = GetBool(PlayerPrefs.GetInt("VRNose"));
+        lowFoV = GetBool(PlayerPrefs.GetInt("lowFoV"));
+    }
+   
+    private bool GetBool(int value)
+    {
+        if(value == 0)
+        {
+            return false;
+        }
+
+        else if(value == 1)
+        {
+            return true;
+        }
+
+        else
+        {
+            return false;
+        }
+    }
+
+    //set the settings to the player
+    public void SetSettings()
+    {
+
+    }
+}
